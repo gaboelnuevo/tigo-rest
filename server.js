8bc792534754c0805ac056241bd9f9b48d1785c7
Gabriel Sabillon <gaboelnuevo@gmail.com>
2017-07-15 12:10:13 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,70 @@
+
+var express = require('express');
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+var request = require("request");
+
+var baseUrl = 'https://testapi.tigo.com.hn/mfsapi/';
+
+app.post('/SalaryPaymentService', function(req, res){
+  var url = baseUrl + 'SalaryPaymentService?WSDL';
+  var args =  req.body.args || {};
+  var options = req.body.options || {};
+  var options = {
+    method: 'POST',
+    url: 'https://testapi.tigo.com.hn/mfsapi/SalaryPaymentService',
+    qs: {
+      WSDL: ''
+    },
+    headers: {
+      'content-type': 'text/xml'
+    },
+    body: `
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v1="http://xmlns.tigo.com/MFS/SalaryPaymentRequest/V1" xmlns:v3="http://xmlns.tigo.com/RequestHeader/V3" xmlns:v2="http://xmlns.tigo.com/ParameterType/V2">
+       <soapenv:Header/>
+       <soapenv:Body>
+          <v1:SalaryPaymentRequest>
+             <v3:RequestHeader>
+                <v3:GeneralConsumerInformation>
+                   ${
+                     Object.keys(options).map(
+                       key => {
+                         return `<v3:${key}>${options[key]}</v3:${key}>`
+                       }
+                     )
+                   }
+                </v3:GeneralConsumerInformation>
+             </v3:RequestHeader>
+             <v1:requestBody>
+                ${
+                  Object.keys(args).map(
+                    key => {
+                      return `<v1:${key}>${args[key]}</v1:${key}>`
+                    }
+                  )
+                }
+             </v1:requestBody>
+          </v1:SalaryPaymentRequest>
+       </soapenv:Body>
+    </soapenv:Envelope>`
+  };
+
+  request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+});
+
+app.listen(PORT, function (){
+   console.log(`App listening on port ${PORT}!`);
+});
